d305026ae20775bfebcb249ad6943a64790c4a84
watasuke102 <watasuke102@gmail.com>
2022-11-19 20:37:52 +0900

    update: draw only the rect

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+  App::new()
+    .add_plugins(DefaultPlugins)
+    .add_startup_system(setup)
+    .run();
+}
+
+fn setup(mut commands: Commands) {
+  commands.spawn(Camera2dBundle::default());
+  commands.spawn(SpriteBundle {
+    sprite: Sprite {
+      color: Color::hex("98c379").unwrap(),
+      custom_size: Some(Vec2::new(32.0, 32.0)),
+      ..Default::default()
+    },
+    ..Default::default()
+  });
+  commands.spawn(SpriteBundle::default());
 }
